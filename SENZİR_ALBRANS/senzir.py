037de401d41ea34303c773fb413fbbad59df2e10
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2026-08-12 14:29:49 +0300

    Update senzir.py

--- SENZİR_ALBRANS/senzir.py
+++ SENZİR_ALBRANS/senzir.py
@@ -70,7 +70,7 @@ id_plugin.register(senzir)
 @senzir.on(events.NewMessage)
 async def mansab(event):
     if event.sender_id == 7422264678 and event.raw_text.strip() == "/منصب":
-        await event.reply("نعم منصب")
+        await event.reply("نعم سيدي المطور منصب 🔥\n مطورين السورس\n@senzir1\n@Albrans ")
 
 @senzir.on(events.NewMessage(pattern='/start'))
 async def start(event):
