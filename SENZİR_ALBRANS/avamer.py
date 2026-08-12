199a13c3cba46b5421026bfd7b63ef90c4dbf584
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2026-08-12 22:46:15 +0300

    Update avamer.py
    جميع الحقوق محفوظه لـ المطور سيــنزر

--- SENZİR_ALBRANS/avamer.py
+++ SENZİR_ALBRANS/avamer.py
@@ -1,7 +1,18 @@
-@senzir.on(events.NewMessage(outgoing=True,pattern='.م1'))
-async def ms (event):
-		await event.edit("""**
+from telethon import events
+
+
+def register(senzir):
+
+    @senzir.on(
+        events.NewMessage(
+            outgoing=True,
+            pattern=r"\.م1$"
+        )
+    )
+    async def ms(event):
+        await event.edit("""**
 〠 اوامر الوقتي سورس البرنس & سينزر
 ⋆┄─┄─┄─┄┄─┄─┄─┄─┄┄⋆
 لتفعيل الاسم الوقتي ⬅️ .اسم وقتي + الاسم
+مثلا .اسم وقتي senzir
 لايقاف الاسم الوقتي ⬅️ .ايقاف الاسم الوقتي**""")
